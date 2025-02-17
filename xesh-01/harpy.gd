2ff74c04c4aab7e6047a6e1023e2e9891262917a
Shinsam <KiraShinin@gmail.com>
2025-02-17 09:46:53 +0100

    Fixed small jumps

--- xesh-01/harpy.gd
+++ xesh-01/harpy.gd
@@ -3,7 +3,7 @@ var move = Vector2(0, 0)
 var gravity = 1
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 25.0
+const JUMP_VELOCITY = 20.0
 const MAXFALLSPEED = 50
 
 func _ready():
@@ -18,7 +18,7 @@ func _physics_process(delta):
 		move.y = MAXFALLSPEED
 	if Input.is_action_pressed("move_up"):
 		if is_on_floor():
-			move.y -= JUMP_VELOCITY
+			move.y = -JUMP_VELOCITY
 	if Input.is_action_pressed("move_left"):
 		move.x -= SPEED
 	if Input.is_action_pressed("move_right"):
@@ -32,5 +32,5 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = move.x < 0
 	else:
 		$AnimatedSprite2D.animation = "Idle"
-	if move.y < 0 || !is_on_floor():
+	if (move.y < 0) || !is_on_floor():
 		$AnimatedSprite2D.animation = "Fly"
