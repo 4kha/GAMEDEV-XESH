8c53adfff3f79578406dfe6a36afb4a3bafb2f9f
Céline Tran <cceline74@hotmail.fr>
2025-02-18 17:08:22 +0100

    Fix ground collision not duplicated

--- xesh-01/scene/forest.gd
+++ xesh-01/scene/forest.gd
@@ -9,3 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Environment/Camera2D.position = $Environment/Harpy.position
+	$Environment/StaticBody2D.position = Vector2($Environment/Camera2D.position.x, $Environment/StaticBody2D.position.y)
