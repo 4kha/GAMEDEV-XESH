a62c8c47bea2258319dd65e4a1e7113298984f59
Céline Tran <cceline74@hotmail.fr>
2025-02-20 15:20:00 +0100

    update script + enhanced level

--- xesh-01/scene/forest.gd
+++ xesh-01/scene/forest.gd
@@ -1,12 +1,16 @@
 extends Node2D
-
+var player
+var camera
+var basicGround
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player = get_node("Harpy")
+	camera = get_node("Camera2D")
+	basicGround = get_node("StaticBody2D")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Environment/Camera2D.position = $Environment/Harpy.position
-	$Environment/StaticBody2D.position = Vector2($Environment/Camera2D.position.x, $Environment/StaticBody2D.position.y)
+	camera.position = player.position
+	basicGround.position = Vector2(camera.position.x, basicGround.position.y)
